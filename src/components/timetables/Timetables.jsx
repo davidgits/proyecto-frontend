d21053cbf33455a77b1a0a698bb5b0cade5e1bed
davidgits <trufa80@gmail.com>
2021-06-21 22:50:51 +0200

    cleaning code

--- src/components/timetables/Timetables.jsx
+++ src/components/timetables/Timetables.jsx
@@ -10,64 +10,6 @@ import './timetables.css'
 export default function Timetables() {
 
     const timetables = timeTablesData;
-
-    // const timetables = [
-
-    //     {
-    //         id: 1,
-    //         title: "Aikido",
-    //         groups: [
-    //             {
-    //                 id: 1,
-    //                 name: "Adultos",
-    //                 days: "Lun, Mie, Vie",
-    //                 time: "18:30 - 19:30",
-    //             },
-    //             {
-    //                 id: 2,
-    //                 name: "Niños (5-12 años)",
-    //                 days: "Mar, Jue",
-    //                 time: "18:30 - 19:30",
-    //             }
-    //         ],
-    //         image: "/assets/images/aiki1.jpg",
-    //         tag: "nav-aiki"
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "Karate",
-    //         groups: [
-    //             {
-    //                 id: 1,
-    //                 name: "Adultos",
-    //                 days: "Lun, Mie, Vie",
-    //                 time: "17:30 - 18:30",
-    //             },
-    //             {
-    //                 id: 2,
-    //                 name: "Niños (5-12 años)",
-    //                 days: "Mar, Jue",
-    //                 time: "17:30 - 18:30",
-    //             }
-    //         ],
-    //         image: "/assets/images/karate2.jpg",
-    //         tag: "nav-karate"
-    //     },
-    //     {
-    //         id: 3,
-    //         title: "Meditación Zen",
-    //         groups: [
-    //             {
-    //                 id: 1,
-    //                 name: "Adultos",
-    //                 days: "Sab",
-    //                 time: "9:00 - 10:00",
-    //             }
-    //         ],
-    //         image: "/assets/images/meditation.jpg",
-    //         tag: "nav-zen"
-    //     },
-    // ]
     
     return (
         <section className="timetables">
